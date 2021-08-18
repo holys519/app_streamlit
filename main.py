172f69738d51c5dc455a004a1a575676ba5493d1
holys519 <holys519@gmail.com>
2021-08-18 14:25:27 +0900

    3 commit

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredenti
 
 def get_tags(filepath):
 
-    local_image = open(local_image_path, "rb")
+    local_image = open(filepath, "rb")
 
 
     tags_result = computervision_client.tag_image_in_stream(local_image)
@@ -24,31 +24,49 @@ def get_tags(filepath):
     tags_name = []
     for tag in tags:
         tags_name.append(tag.name)
-        
+
     return tags_name
 
 def detect_objects(filepath):
 
     local_image = open(filepath, "rb")
-    
+
     detect_objects_results = computervision_client.detect_objects_in_stream(local_image)
     objects = detect_objects_results.objects
     return objects
 
 
-import streamlit as streamlit
+import streamlit as st
+from PIL import ImageDraw
+from PIL import ImageFont
 st.title('物体検出アプリ')
 
 uploaded_file = st.file_uploader ('choose as image...', type=['jpg', 'png'])
 if uploaded_file is not None:
-    Image.open(uploaded_file)
-    img_path = f'img/{upload_file.name}'
+    img = Image.open(uploaded_file)
+    img_path = f'img/{uploaded_file.name}'
     img.save(img_path)
+    objects = detect_objects(img_path)
+    # 描画
+    draw = ImageDraw.Draw(img)
+    for object in objects:
+        x = object.rectangle.x
+        y = object.rectangle.y
+        w = object.rectangle.w
+        h = object.rectangle.h
+        caption = object.object_property
+
+        font = ImageFont.truetype(font='./Helvetica 400.ttf', size=50)
+        text_w, text_h = draw.textsize(caption, font=font)
+
+        draw.rectangle([(x, y),(x+w, y+h)], fill=None, outline='green', width=5)
+        draw.rectangle([(x, y),(x+text_w, y+text_h)], fill='green', outline='green')
+        draw.text((x,y), caption, fill='white', font=font)
+
+
     st.image(img)
 
-
-
-
-
+    tags_name = get_tags(img_path)
+    tags_name = ','.join(tags_name)
     st.markdown('**認識されたコンテンツタグ**')
-    st.markdown('> apple, tree, building, green')
+    st.markdown(f'> {tags_name}')
